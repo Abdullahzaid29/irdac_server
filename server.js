4c63c6373d9f8bc36bed354705c4ce75ffef7fb5
unknown <abdullahzaid2904@gmail.com>
2023-08-19 13:13:18 +0530

    cors try3

--- server.js
+++ server.js
@@ -12,48 +12,15 @@ dotenv.config({path:"config/config.env"})
 //     methods: 'GET,PUT,POST,DELETE',   // List of allowed HTTP methods
 //     optionsSuccessStatus: 200
 // }
-// var corOptions = {
-//   origin:["https://irdai-dashboard.vercel.app","http://localhost:5000"],
-//   credentials: true,
-//   optionsSuccessStatus: 200
-// }
+var corOptions = {
+  origin:["https://irdai-dashboard.vercel.app","http://localhost:5000"],
+  credentials: true,
+  optionsSuccessStatus: 200
+}
 
 
-// app.use(cors(corOptions))
-// app.use(cors({ origin: true }));
+app.use(cors(corOptions))
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:5000"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-app.options("*", (req, res) => {
-  console.log("preflight");
-  if (
-    req.headers.origin === "http://localhost:5000" &&
-    allowMethods.includes(req.headers["access-control-request-method"]) &&
-    allowHeaders.includes(req.headers["access-control-request-headers"])
-  ) {
-    console.log("pass");
-    return res.status(204).send();
-  } else {
-    console.log("fail");
-  }})
 app.use(express.json())
 
 app.use(express.urlencoded({ extended:true }))
